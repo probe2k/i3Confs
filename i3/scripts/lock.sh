35218ec093f218e2393512864942afec37c4e461
probe2k <kyash054@gmail.com>
2021-01-02 18:32:39 +0530

    lock.sh modified | ffmpeg boxblur:luma_redef enc redundancy

--- i3/scripts/lock.sh
+++ i3/scripts/lock.sh
@@ -6,6 +6,8 @@ RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x
 #ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=10:5,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
 #i3lock -i $TMPBG
 
+#ffmpeg -f x11grab -i $DISPLAY -i $LOCK -filter_complex "[0:v]boxblur=luma_radius=min(w\,h)/5:chroma_radius=min(cw\,ch)/5:luma_power=1[blurred],overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -map "[blurred]" -vframes 1 $TMPBG -loglevel quiet
+
 ffmpeg -f x11grab -i $DISPLAY -i $LOCK -filter_complex "gblur=100,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
 i3lock -i $TMPBG
 rm $TMPBG
